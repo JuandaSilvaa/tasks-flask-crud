efd29dba54ad11bd503bd85a3d09b251df89f9c7
JuandaSilvaa <almeidajuan529@gmail.com>
2025-03-15 17:33:39 -0300

    Add crud and class task

--- app.py
+++ app.py
@@ -1,17 +1,115 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from models.task import Task
 
-# __name__ = '__main__ quando roda de forma manual
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-  return "Hello world!"
+# CRUD
+# Create, Read, Update and Delete = Criar, Ler, Atualizar, Deletar
+# Tabela: Tarefa
 
-@app.route("/about")
-def about():
-  return "Página sobre"
+tasks = []
+task_id_control = 1
 
-# debug=True habilita os logs
-# isso e so para desenvolvimento local, quando rodamos de forma manual
+
+@app.route("/tasks", methods=["POST"])
+def create_task():
+    global task_id_control  # para usar a variavel q esta fora para controle
+    data = request.get_json()
+    new_task = Task(
+        id=task_id_control, title=data["title"], description=data.get(
+            "description", "")
+    )
+    task_id_control += 1
+    tasks.append(new_task)
+    print(tasks)
+    return jsonify({"message": "Nova tarefa criada com sucesso"})
+
+
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+    task_list = [task.to_dict() for task in tasks]
+    output = {
+        "tasks": task_list,
+        "total_tasks": len(task_list)
+    }
+    return jsonify(output)
+
+
+@app.route('/tasks/<int:id>', methods=['GET'])
+def get_task(id):
+    for t in tasks:
+        if t.id == id:
+            return jsonify(t.to_dict())
+    return jsonify({"message": "Não foi possivel encontar a atividade"}), 404
+
+
+@app.route('/tasks/<int:id>', methods=['PUT'])
+def update_task(id):
+    task = None
+    for t in tasks:
+        if t.id == id:
+            task = t
+            break # quebra o loop quando achar pra n ficar percorrendo todos mesmo apos achar 
+
+    if task == None:
+        return jsonify({"message": "Não foi possivel encontrar a atividade"}), 404
+
+    data = request.get_json()
+    task.title = data['title']
+    task.description = data['description']
+    task.completed = data['completed']
+    return jsonify({"message": "Tarefa atualizada com sucesso"})
+
+
+@app.route('/tasks/<int:id>', methods=['DELETE'])
+def delete_task(id):
+    task = None
+    for t in tasks:
+        if t.id == id:
+            task = t
+            break
+
+    if not task:
+        return jsonify({"message": "Não foi possivel encontrar a atividade"}), 404
+
+    tasks.remove(task)
+    return jsonify({"message": "Tarefa deletada com sucesso"})
+
 if __name__ == "__main__":
-  app.run(debug=True)
+    app.run(debug=True)
+
+# Rota test de parametro
+# Documentação flask.palletsprojects.com/en/3.0.x/quickstart/#routing
+"""@app.route('/user/<username>')
+def show_user(username):
+    print(username)
+    print(type(username))
+    return username"""
+
+
+# forma verbosa GET
+"""@app.route('/tasks', methods=['GET'])
+def get_tasks():
+    task_list = []
+    for task in tasks:
+        task_list.append(task.to_dict())
+    output = {
+
+        "tasks": task_list,
+        "total_tasks": 0
+    }
+    return jsonify(output)"""
+
+
+# tipos de parametros
+
+"""string (default) accepts any text without a slash
+
+int accepts positive integers
+
+float accepts positive floating point values
+
+path like string but also accepts slashes
+
+uuid accepts UUID strings
+"""
